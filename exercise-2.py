1cfae05acf2102f510fcd6466d72cd44653be156
virakyuthSRUN <2023494srun@aupp.edu>
2024-03-17 17:47:43 +0700

    updated

--- exercise-2.py
+++ exercise-2.py
@@ -1,2 +1,18 @@
-def index_power(numnbers, n):
-    ...
+def index_power(numbers, n):
+
+    # check if n is less than the length of the list    
+    if n < len(numbers):
+
+        # calculate the power of the number at index n
+        result = numbers[n] ** n
+        return result
+    
+    # return -1 if n is greater than the length of the list
+    else:
+        return -1
+    
+# Test cases
+print(index_power([1, 2, 3, 4], 2)) # 9
+print(index_power([1, 3, 10, 100], 3)) # 1000000
+print(index_power([0, 1], 0)) # 1
+print(index_power([1, 2], 3)) # -1
