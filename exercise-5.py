65f43f03a7dff7423c8de2ed308ead28477ba458
virakyuthSRUN <2023494srun@aupp.edu>
2024-03-17 17:09:58 +0700

    Update exercise-5.py

--- exercise-5.py
+++ exercise-5.py
@@ -1,14 +1,24 @@
 def reverse_ascending(items):
+    # return nothing if the list is empty
     if not items:
-        return
+        return []
 
+    # create a list to store the reversed subsequence
     current_subseq = [items[0]]
+
+    # iterate through the list and check if the current item is greater than the previous item
     for i in range(1, len(items)):
         if items[i] > items[i - 1]:
+
+            # append the current item to the current subsequence
             current_subseq.append(items[i])
+        
+        # if the current item is less than the previous item, yield the reversed subsequence
         else:
             yield from reversed(current_subseq)
             current_subseq = [items[i]]
+    
+    # yield the reversed subsequence
     yield from reversed(current_subseq)
 
 # example
