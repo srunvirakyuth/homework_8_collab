e364784682e28d94dd5ec6ecf04cb6d3a18f5be3
virakyuthSRUN <2023494srun@aupp.edu>
2024-03-17 15:57:41 +0700

    exercise 2 solution

--- exercise-2.py
+++ exercise-2.py
@@ -1,2 +1,12 @@
-def index_power(numnbers, n):
-    ...
+def index_power(numbers, n):
+
+    # check if n is less than the length of the list    
+    if n < len(numbers):
+
+        # calculate the power of the number at index n
+        result = numbers[n] ** n
+        return result
+    
+    # return -1 if n is greater than the length of the list
+    else:
+        return -1
--- exercise-5.py
+++ exercise-5.py
@@ -1,2 +1,18 @@
-def reverse_ascending(numbers):
-    ...
+def reverse_ascending(items):
+    if not items:
+        return
+
+    current_subseq = [items[0]]
+    for i in range(1, len(items)):
+        if items[i] > items[i - 1]:
+            current_subseq.append(items[i])
+        else:
+            yield from reversed(current_subseq)
+            current_subseq = [items[i]]
+    yield from reversed(current_subseq)
+
+# example
+print(list(reverse_ascending([1, 2, 3, 4, 5])))
+print(list(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3])))
+print(list(reverse_ascending(([5, 4, 3, 2, 1]))))
+print(list(reverse_ascending([])))
